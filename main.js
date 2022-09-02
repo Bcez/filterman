d4affa4f2652e4a180f81c9d77369716ae198df0
Bcez <80232341+Bcez@users.noreply.github.com>
2022-09-02 14:21:28 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-function preload(){
+noseX=0;
+noseY=0;
 
+
+
+
+function preload(){
+clown_nose = loadImage('https://i.postimg.cc/3x3QzSGq/m.png')
 }
 
 function setup() {
@@ -22,14 +28,18 @@ function modelLoaded()
 function gotPoses(results) 
 {
     if(results.length > 0){
-    console.log(results);
-    console.log("nose x  =" + results[0].pose.nose.x);
-    console.log("nose y  =" + results[0].pose.nose.y);
+   console.log(results);
+   noseX = results[0].pose.nose.x;
+   noseY = results[0].pose.nose.y;
+   console.log("nose x = " + noseX);
+   console.log("nose y = " + noseY);
   }
 }
 function draw(){
 image(video, 0, 0, 300, 300);
+image(mustache, noseX-15, noseY-10, 30, 30);
 }
+
 function take_snapshot(){
-    save('rickroll69.png');
+    save('MyFilterImage.png');
 }
